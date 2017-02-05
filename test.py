c49dc29537620259add6aa9f5029a75132e03672
kantehabdallah <bt16005@qmul.ac.uk>
2017-02-05 06:34:19 -0800

    update test.py

--- test.py
+++ test.py
@@ -1,13 +1,17 @@
-from flask import Flask, request, url_for, render_template, redirect
+from flask import Flask, request, url_for, render_template, redirect, flash, send_from_directory
 import os
 from werkzeug.utils import secure_filename
 
-upload_folder = '/home/abdou/myproject/app/fastafiles'
+
+upload_folder = '/home/abdou/myproject/uploads'
 allowed_extensions = set(['fa', 'fasta'])
 
+# initialise the Flask application
 app = Flask(__name__)
 
-app.config['upload_folder']= upload_folder
+app.config['UPLOAD_FOLDER']= upload_folder
+app.config['ALLOWED_EXTENSIONS'] =allowed_extensions
+app.secret_key= "85QSjmqj"
 
 
 def allowed_file(filename):
@@ -19,30 +23,41 @@ def index():
     return render_template("about.html")
 
 
-
 @app.route('/blast')
-def upload_filepost():
+def upload_file():
  return render_template("upload.html")
 
 
-
-@app.route('/blast', methods = ['GET', 'POST'])
-def upload_file():
- 
- if request.method == 'POST': # check if the post request has the file path
-  if 'file' not in request.files:
-    flash('no file path')
-    return redirect(request.url) # if user does not select file, browser also submit a an empty part without filename
- file=request.files['file']
-
- if file.filename == '':
-  flash('no selected file')
-  return redirect (request.url)
-
+@app.route('/blast', methods= ['POST'])
+def upload():
+ # get th name of the uploaded file
+ file= request.files['file']
+ #check if the file is one of the extensions types allowed
  if file and allowed_file(file.filename):
-  filename= secure_filename(file.filename)
-  file.save(os.path.join(app.config['upload_folder'], filename))
-  return redirect(url_for('uploaded_file', filename=filename))
+ #make the file safe, remove unsupported chars
+  filename=secure_filename(file.filename)
+ 
+ #move the file from the temporal folder to the upload folder we setup
+
+  file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename)
+)
+ # redirect the user to the uploaded_file route which will basically show on the browser the uploaded file
+
+  return redirect(url_for("uploaded_file", 
+                           filename=filename))
+
+
+
+@app.route('/uploads <filename>')
+def uploaded_file(filename):
+ return send_from_directory(upload_folder, filename)
+
+
+  
+
+
+
+ 
 
 
 
