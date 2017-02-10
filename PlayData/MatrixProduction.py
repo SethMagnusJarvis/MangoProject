bedceee0acff0509c548b0dc8387e47c1b1647cc
SethMagnusJarvis <s.m.jarvis@se16.qmul.ac.uk>
2017-02-10 16:24:20 +0000

    Accidentally used older version

--- PlayData/MatrixProduction.py
+++ PlayData/MatrixProduction.py
@@ -3,6 +3,22 @@ import glob, os, shutil
 import numpy
 import math
 
+def fixsort(data):
+	#drops NA values
+	data = data.dropna(axis=0, how='any')
+	#sorts columns by Accession then by FPKM
+	data = data.sort_values(by=['Accession', 'FPKM'])
+	#removes instance of Accession which appears becasue of the way files are imported
+	data = data[data.Accession != 'Accession']
+	#checks every record to see if the first value is a number and removes those which are (weird quirk again)
+	for x in data.Accession:
+		listy = list(x)
+		if listy[0] == '1' or listy[0] == '2' or listy[0] == '3'or listy[0] == '4'or listy[0] == '5'or listy[0] == '6'or listy[0] == '7'or listy[0] == '8'or listy[0] == '9' :
+			data = data[data.Accession != x]
+	#drops duplicate ascession numbers keeping one with highest FPKM
+	data = data.drop_duplicates(subset='Accession')
+	return(data)
+
 #move files from upload to CSV for easier future use
 source_dir = 'Upload/'
 path = 'CSV/'
@@ -44,3 +60,8 @@ while x<length:
 		#breaks the loop becasue ascession is the last incorrect value (It's possible that this could be made more effecient by using a for loop but when I started I was using indexing)
 		break
 	x=x+1
+
+for file in files: 
+	data = pd.read_csv(path+file, error_bad_lines=False, names=mycols)
+	data = fixsort(data)
+
--- PlayData/PlayTest.py
+++ PlayData/PlayTest.py
@@ -10,27 +10,6 @@ AccessionList = []
 mycols = ['Accession', 'FPKM']
 data = pd.read_csv('CSV/G1S1.csv', error_bad_lines=False, names=mycols)
 
-def frange(start, end=None, inc=None):
-    "A range function, that does accept float increments..."
-
-    if end == None:
-        end = start + 0.0
-        start = 0.0
-
-    if inc == None:
-        inc = 1.0
-
-    L = []
-    while 1:
-        next = start + len(L) * inc
-        if inc > 0 and next >= end:
-            break
-        elif inc < 0 and next <= end:
-            break
-        L.append(next)
-        
-    return L
-
 def fixsort(data):
 	#drops NA values
 	data = data.dropna(axis=0, how='any')
@@ -38,17 +17,20 @@ def fixsort(data):
 	data = data.sort_values(by=['Accession', 'FPKM'])
 	#removes instance of Accession which appears becasue of the way files are imported
 	data = data[data.Accession != 'Accession']
+	#checks every record to see if the first value is a number and removes those which are (weird quirk again)
 	for x in data.Accession:
 		listy = list(x)
 		if listy[0] == '1' or listy[0] == '2' or listy[0] == '3'or listy[0] == '4'or listy[0] == '5'or listy[0] == '6'or listy[0] == '7'or listy[0] == '8'or listy[0] == '9' :
 			data = data[data.Accession != x]
 	#drops duplicate ascession numbers keeping one with highest FPKM
 	data = data.drop_duplicates(subset='Accession')
-	print(data)	
+	return(data)	
 
-
-data=fixsort(data)
-
+for file in files: 
+	data = pd.read_csv(path+file, error_bad_lines=False, names=mycols)
+	data = fixsort(data)
+	
+	
 
 
 
