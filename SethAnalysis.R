68aa87c22ecfd4b6995149cc7a5050cd480497b6
SethMagnusJarvis <s.m.jarvis@se16.qmul.ac.uk>
2017-02-11 19:49:16 +0000

    Commented code

--- SethAnalysis.R
+++ SethAnalysis.R
@@ -1,16 +1,27 @@
+library(limma)
 
+#load data
 data <- as.matrix(read.csv("CSV/DataSet.csv"))
-list <- data[,1]
-data <- data.matrix(read.csv("DataSet.csv"))
-row.names(data) <- list
-data <- data[,-1]
-logdata <- log2(data)
-logdata
 
+#produce a list of ascession numbers
+list <- data[,1]
+
+#turn the data into a data matrix
+data <- data.matrix(read.csv("DataSet.csv"))
+
+#add ascession numbers as row names
+row.names(data) <- list
+
+#remove weird random column which I think is a numerical interpretation of the Ascession code
+data <- data[,-1]
+
+#log the matrix
+logdata <- log2(data)
+
+#produce an expression set from the data
 ExpSet <- ExpressionSet(assayData=logdata)
 
-
-
+#currently useless, prooduces something which isn't any good
 pca <- prcomp(logdata, centre=TRUE, scale.=TRUE)
 pca
 plot(pca, type = "l")
