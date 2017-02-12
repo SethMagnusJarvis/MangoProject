2fdb07d1775e0e1681babe271e95afba92251690
SethMagnusJarvis <s.m.jarvis@se16.qmul.ac.uk>
2017-02-12 19:15:56 +0000

    Updated analysis with working top table

--- PlayData/SethAnalysis.R
+++ PlayData/SethAnalysis.R
@@ -1,6 +1,7 @@
 library("limma")
+library("ggplot2")
 
-setwd("D:/MangoProject/PlayData")
+setwd(getwd())
 
 #load data
 data <- as.matrix(read.csv("CSV/DataSet.csv"))
@@ -21,17 +22,33 @@ data <- data[,-1]
 logdata <- log2(data)
 
 #produce a model matrix
-group <- factor(c(1,1,1,2,2,2,3,3,3))
+design <- model.matrix(~ 0+factor(c(1,1,1,2,2,2,3,3,3)))
+colnames(design) <- c("Uninfected", "EightHours", "OneDay")
 
-#currently looking at using http://genomicsclass.github.io/book/pages/expressing_design_formula.html for help
 
 #produce an expression set from the data
-ExpSet <- ExpressionSet(assayData=logdata)
+eset <- ExpressionSet(assayData=logdata)
 
-#doesn't work, says "Error in NCOL(y) : argument "y" is missing, with no default"
-lm <- lm.fit(ExpSet)
+#Produces a linear model
+fit <- lmFit(eset, design)
 
-#currently useless, prooduces something which isn't any good
-pca <- prcomp(logdata, centre=TRUE, scale.=TRUE)
-pca
-plot(pca, type = "l")
+contrast.matrix <- makeContrasts(EightHours-Uninfected, OneDay-Uninfected, OneDay-EightHours, levels=design)
+fit2 <- contrasts.fit(fit, contrast.matrix)
+fit2 <- eBayes(fit2)
+
+#produce a matrix of the top differentially expressed genes
+#Uninfected vs 8 hours
+topTable(fit2, coef=1, adjust="BH")
+
+#Uninfected vs 24 hours
+topTable(fit2, coef=2, adjust="BH")
+
+#8 Hours vs 24 hours
+topTable(fit2, coef=1, adjust="BH")
+
+results <- decideTests(fit2)
+
+vennDiagram(results)
+
+plot(eset)
+
