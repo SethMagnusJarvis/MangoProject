8708fe5107dcbd2a25665e30a39720475b341960
SethMagnusJarvis <s.m.jarvis@se16.qmul.ac.uk>
2017-02-15 00:13:17 +0000

    Commented R code

--- PlayData/SethAnalysis.R
+++ PlayData/SethAnalysis.R
@@ -1,40 +1,34 @@
+#load libraries
 library("limma")
 library("Biobase")
-#library("calibrate")
 
-setwd(getwd())
-
+#correctly load the data as a matrix
 #load data
 data <- as.matrix(read.csv("CSV/DataSet(1).csv"))
-
 #produce a list of ascession numbers
 list <- data[,1]
-
 #turn the data into a data matrix
 data <- data.matrix(read.csv("CSV/DataSet(1).csv"))
-
 #add ascession numbers as row names
 row.names(data) <- list
-
 #remove weird random column which I think is a numerical interpretation of the Ascession code
 data <- data[,-1]
-
 #add sample names as col names
 colnames(data)<-c("G1S1", "G1S2", "G1S3","GS1", "G2S2", "G2S3","G3S1", "G3S2", "G3S3")
-
 #log the matrix
 logdata <- log2(data)
 
-#produce a model matrix
+#produce a model matrix, setting 3 sample groups and naming them
 design <- model.matrix(~ 0+factor(c(1,1,1,2,2,2,3,3,3)))
 colnames(design) <- c("Uninfected", "EightHours", "OneDay")
 
-#produce an expression set from the data
+#produce an expression set from the matrix
 eset <- ExpressionSet(assayData=logdata)
 
 #Produces a linear model
 fit <- lmFit(eset, design)
 
+#creates the contrast matrix then looks for emperical Bayes method to examine variance
 contrast.matrix <- makeContrasts(EightHours-Uninfected, OneDay-Uninfected, OneDay-EightHours, levels=design)
 fit2 <- contrasts.fit(fit, contrast.matrix)
 fit2 <- eBayes(fit2)
